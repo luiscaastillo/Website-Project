a84cd566578096809ce7091101c10bbd7eaaef70
Luis Castillo <luiseduardocastilloruvalcaba@gmail.com>
2026-05-06 16:35:50 +0200

    Update API port from 3001 to 3000 in server and client configuration

--- public/js/firebase-crud.js
+++ public/js/firebase-crud.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3001/api/data';
+const API_BASE_URL = 'http://localhost:3000/api/data';
 
 // ---------------------------------------------------------
 // 1. CREATE (Crear o sobrescribir datos puntuales)
